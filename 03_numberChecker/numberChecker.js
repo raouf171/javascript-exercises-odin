b1620d1d70e9e04b8f5edd5040c5ae629c3f0c06
RAOUFbenedine <raoufbenedine@gmail.com>
2025-07-24 01:51:59 +0100

    exo4 solved

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,5 +1,7 @@
 function numberChecker(number) {
-  if (number === 6) {
+  //Modify the code so it returns `true` when the number is greater than or equal to 10, and `false` if it is less than 10.
+
+  if (number >=10) {
     return true;
   } else {
     return false;
